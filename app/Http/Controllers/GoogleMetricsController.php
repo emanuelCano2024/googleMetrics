1576fb8c66d7d25c7dec4e4b56c151d281b6ad12
ema <ecano9299@gmail.com>
2024-09-23 03:41:06 -0300

    completando challenge

---
+++ app/Http/Controllers/GoogleMetricsController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Strategy;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use App\Models\MetricHistoryRun;
+
+class GoogleMetricsController extends Controller
+{
+    public function index()
+    {
+        return view('metricsGoogle.index',['categories'=>[],'strategies'=>[]]);
+    }
+
+    private function validarData($request) 
+    {
+        $validated = $request->validate([
+            'urlWeb' => 'required|url',
+        ], [
+            'urlWeb.required' => 'El campo URL es obligatorio.',
+            'urlWeb.url' => 'El campo debe contener una URL valida.',
+        ]);
+        return $validated;
+    }
+
+    private function infoUsuarioVista($metricasResultado) 
+    {
+        $result = [];
+        $strokeDashArray = 408;
+        foreach ($metricasResultado as $key => $value) {
+            $percentage = $value * 100;
+            $strokeDashoffset = $strokeDashArray - ($strokeDashArray * ($value));
+            $result[$key] = [
+                'percentage'=> $percentage,
+                'strokeDashoffset'=> $strokeDashoffset,
+                'value'=> $value
+            ];
+        }
+        return $result;
+    }
+
+    private function getMetricas($validated) 
+    {
+        try {
+            $url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?'
+            . 'url=https://broobe.com&'
+            . 'key=AIzaSyDCrPAzhzWxZbJxPYIEURODTvBFVVRNHbY&'
+            . 'category=SEO&'
+            . 'category=PERFORMANCE&'
+            . 'category=BEST_PRACTICES&'
+            . 'category=ACCESSIBILITY&'
+            . 'strategy=mobile';
+            $client = new Client(); 
+            $response = $client->request('GET', $url);
+            $data = json_decode($response->getBody()->getContents(), true);
+            $metricasResultado= [
+                'seo'=> $data['lighthouseResult']['categories']['seo']['score'],
+                'performance'=> $data['lighthouseResult']['categories']['performance']['score'],
+                'bestPractices'=> $data['lighthouseResult']['categories']['best-practices']['score'],
+                'accessibility'=> $data['lighthouseResult']['categories']['accessibility']['score']
+            ];
+            // $metricasResultado= [
+            //         'seo'=> 0.85,
+            //         'performance'=> 0.20,
+            //         'bestPractices'=> 0.45,
+            //         'accessibility'=> 0.94
+            // ];
+            return $this->infoUsuarioVista($metricasResultado);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function process(Request $request)
+    {
+        $validated = $this->validarData($request);
+        $metricas= $this->getMetricas($validated);
+        return view('metricsGoogle.indicadores',['metricas'=>$metricas]);
+    }
+
+    public function save(Request $request)
+    {
+        $performanceValue = $request->input();
+        $validated = $request->validate([
+            'url'=> 'required|url',
+            'strategy_id'=> 'required|numeric',
+            'seo' => 'required|numeric|between:0,1',
+            'performance' => 'required|numeric|between:0,1',
+            'best' => 'required|numeric|between:0,1',
+            'access' => 'required|numeric|between:0,1',
+        ]);
+
+        MetricHistoryRun::create([
+            'url' => $request->input('url'),
+            'accesibility_metric' => $validated['access'],
+            'pwa_metric' => 0.0, 
+            'performance_metric' => $validated['performance'],
+            'seo_metric' => $validated['seo'],
+            'best_practices_metric' => $validated['best'],
+            'strategy_id' => $request->input('strategy_id'),
+        ]);
+    
+        // Return success message or any other appropriate response
+        return response()->json(['message' => 'Metrics saved successfully.']);    
+    }
+
+    public function history() 
+    {
+        $historico = MetricHistoryRun::all()->toArray();
+        return view('metricsGoogle.historialMetricas',['historico'=>$historico]);
+    }
+}
